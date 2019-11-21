f87159d24299bb1a5f64e7b03995ee147d22e232
mdkChaos <mdkchaos1@gmail.com>
2019-11-21 18:38:59 +0100

    Add wwwroot folder and add css and lib subfolders.

--- Forum/Startup.cs
+++ Forum/Startup.cs
@@ -15,6 +15,7 @@ namespace Forum
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,11 +24,15 @@ namespace Forum
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
             }
 
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: null,
+                    template: "{controller}/{action}/{id?}"
+                );
             });
         }
     }
